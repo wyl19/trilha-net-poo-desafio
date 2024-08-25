f451e044242abbcde402f002785a1234ba8793c1
wyl19 <william_ldna@hotmail.com>
2024-08-25 20:56:10 -0300

    Desafio Abstraindo Celular concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Teste Nokia");
+Smartphone nokia = new Nokia(numero:"99999", modelo: "tijolar", imei: "1234", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Olx");
+
+Console.Write($"\n Teste Iphone");
+Smartphone iphone = new Iphone(numero:"88888", modelo: "modelo x", imei: "4321", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
